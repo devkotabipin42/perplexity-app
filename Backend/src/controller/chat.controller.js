42a07690ba290713174fb78eb439a5f1211c31b3
devkotabipin42 <devkotabipin42@.com>
2026-03-21 11:33:22 +0900

    feat: add Mistral AI integration for chat title generation and enhance message handling

--- Backend/src/controller/chat.controller.js
+++ Backend/src/controller/chat.controller.js
@@ -1,5 +1,39 @@
+import chatModel from "../model/chat.model.js"
+import { generateChatTitle, generateResponse } from "../services/ai.services.js"
+import messageModel from '../model/message.model.js'
+
 export async function sendMessage(req,res){
-    const {message} = req.body
-    console.log(message);
+    const {message,chat:chatId} = req.body
     
-}
+    let title= null,chat=null
+    
+    if(!chatId){
+        title = await generateChatTitle(message)
+        chat = await chatModel.create({
+        user:req.user.id,
+        title
+    })
+    }
+    
+    const userMessage = await messageModel.create({
+            chat:chatId || chat._id,
+            content:message,
+            role:'user'
+        })
+        
+    const messages = await messageModel.find({chat:chatId|| chat._id })
+    const result = await generateResponse(messages)
+
+    
+    const aiMessage = await messageModel.create({
+        chat:chatId || chat._id,
+        content:result,
+        role:'ai'
+    })
+    
+
+    res.status(201).json({
+        title,chat,aiMessage
+    })
+    
+}  
--- Backend/src/services/ai.services.js
+++ Backend/src/services/ai.services.js
@@ -1,20 +1,49 @@
  import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+ import {ChatMistralAI} from '@langchain/mistralai'
+import {AIMessage, HumanMessage,SystemMessage} from 'langchain'
 
-const model = new ChatGoogleGenerativeAI({
+
+
+const geminiModel = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-lite",
   apiKey: process.env.GEMINI_API_KEY
 });
 
-export async function testAi(){
-    model.invoke("what is the tallest animal in thw world").then((response)=>{
-        console.log(response.text);
+const mistralModel = new ChatMistralAI({
+    model : 'mistral-small-latest',
+    apiKey:process.env.MISTRAL_API_KEY
+})
+
+// export async function testAi(){
+//     model.invoke("what is the tallest animal in thw world").then((response)=>{
+//         console.log(response.text);
         
-    })
+//     })
+// }
+
+
+
+export async function generateResponse(messages){
+    const response = await geminiModel.invoke(messages.map(msg=>
+        {
+        if (msg.role=='user'){
+            return new HumanMessage(msg.content)
+        }else if (msg.role=='ai'){
+            return new AIMessage(msg.content)
+        }
+    }
+    ))
+    return response.text
 }
 
-export async function generateResponse(message){
-    const response = await model.invoke([
-        new HumanMessage(message)
-    ])
+export async function generateChatTitle(message){
+    const response = await mistralModel.invoke([
+        new SystemMessage(`You are a helpful assistant that generate concise and descriptive titles for  chat conversation 
+            
+            User will provide you with the first message of a chat conversation and you will generate a title that captures 
+            that essnece of the conversation in 2-4 words.THE title should be clear relevant and engaging giving users a quick
+             understanding of the chat topic` ),
+             new HumanMessage(`Generate a tittle for a chat conversation based on the following first message: "${message}" `)
+])
     return response.text
 }
