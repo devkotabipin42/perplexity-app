a0fbf7163730e20d292441366727431ca1931688
devkotabipin42 <devkotabipin42@.com>
2026-03-24 08:35:27 +0900

    complete

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(morgan('dev'))
 app.use(cors({
-    origin:'http://localhost:5173',
+   origin: [
+        'http://localhost:5173',
+        'https://perplexity-app-six.vercel.app'  
+    ],
     credentials:true,
     methods:['GET','POST','PUT','DELETE']
 }))
