18b89ad482bbd5ecb77284b97e55841cdba6ee53
devkotabipin42 <devkotabipin42@.com>
2026-03-24 10:42:58 +0900

    fix: add interceptor to chat api

--- frontend/src/features/chat/service/chat.api.js
+++ frontend/src/features/chat/service/chat.api.js
@@ -5,7 +5,11 @@ const api = axios.create({
     withCredentials: true,
 })
 
-
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token')
+    if (token) config.headers.Authorization = `Bearer ${token}`
+    return config
+})
 export const sendMessage = async ({ message, chatId, image }) => {
     // ✅ agar image hai toh FormData use karo
     if (image) {
